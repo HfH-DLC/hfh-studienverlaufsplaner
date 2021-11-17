549a13f3b83fea6bfbae3760324df440be76ed33
Matthias Nötzli <matthias.noetzli@gmail.com>
2021-11-17 15:11:18 +0100

    Extract Module Component

---
+++ resources/js/Components/Module.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    class="text-left text-sm disabled:cursor-default mb-2"
+    :disabled="disabled"
+    :class="{
+      'module--selected': selected,
+      'module--disabled': disabled,
+    }"
+  >
+    {{ module.id }} | {{ module.name }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    module: {
+      type: Object,
+      required: true,
+    },
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,36 +1,26 @@
 <template>
-  <div class="flex">
-    <div class="w-2/12 p-4">
+  <div class="flex min-h-screen">
+    <div class="w-3/12 p-4">
       <div v-for="category in plan.categories" :key="category" class="mb-4">
-        <h2 class="text-lg">
-          {{ category.name }} ({{ category.placedNumber }} /
-          {{ category.requiredNumber }})
-        </h2>
+        {{ category.name }} ({{ category.placedNumber }} /
+        {{ category.requiredNumber }})
         <ul>
           <li v-for="module in category.modules" :key="module.id">
-            <button
+            <Module
               @click="toggleSelection(module)"
-              class="text-left disabled:cursor-default mb-2"
+              :module="module"
               :disabled="
                 !plan.hasFreeSlots() ||
                 category.placedNumber === category.requiredNumber ||
                 (module.errors && module.errors.length > 0)
               "
-              :class="{
-                'module--selected': module.id == selectedModuleId,
-                'module--disabled':
-                  !plan.hasFreeSlots() ||
-                  category.placedNumber === category.requiredNumber ||
-                  (module.errors && module.errors.length > 0),
-              }"
-            >
-              {{ module.id }} | {{ module.name }}
-            </button>
+              :selected="module.id == selectedModuleId"
+            />
           </li>
         </ul>
       </div>
     </div>
-    <div class="w-8/12 p-4 bg-gray-100">
+    <div class="w-7/12 p-4 bg-gray-100">
       <ul>
         <li v-for="timeSlot in plan.timeSlots" :key="timeSlot.id">
           <TimeSlot
@@ -54,71 +44,74 @@
 import { ref } from "vue";
 import Plan from "../Models/Plan";
 import Category from "../Models/Category";
-import Module from "../Models/Module";
+import ModuleModel from "../Models/Module";
 import TimeSlotModel from "../Models/TimeSlot";
 import PrerequisiteRule from "../Models/Rules/PrerequisiteRule";
 import OnePerSemesterRule from "../Models/Rules/OnePerSemesterRule";
 
+// Components
 import TimeSlot from "../Components/TimeSlot.vue";
+import Module from "../Components/Module.vue";
 
 export default {
   components: {
     TimeSlot,
+    Module,
   },
   setup() {
     const categories = [
       new Category("Pflichtbereich HFE", [
-        new Module("P1_01", "Grundfragen der Heilpädagogik", 5),
-        new Module("P1_03", "Heilpädagogik im Vorschulbereich", 5),
-        new Module(
+        new ModuleModel("P1_01", "Grundfragen der Heilpädagogik", 5),
+        new ModuleModel("P1_03", "Heilpädagogik im Vorschulbereich", 5),
+        new ModuleModel(
           "P4_02",
           "Grundlagen der Heilpädagogischen Früherziehung",
           5
         ),
-        new Module(
+        new ModuleModel(
           "P4_06",
           "Diagnostik und Früherfassung in der Heilpädagogischen Früherziehung",
           5
         ),
-        new Module(
+        new ModuleModel(
           "P4_07",
           "Entwicklungsorientierte Intervention in der Heilpädagogischen Früherziehung",
           5
         ),
-        new Module(
+        new ModuleModel(
           "P4_08",
           "Beratung und Begleitung von Eltern und weiteren Bezugs- und Fachpersonen in der Heilpädagogischen Früherziehung",
           5
         ),
-        new Module(
+        new ModuleModel(
           "P4_09",
           "Interdisziplinarität und Kooperation im Kontext der Heilpädagogischen Früherziehung",
           5
         ),
       ]),
       new Category("Berufspraxis HFE", [
-        new Module("BP5_01.1.HFE", "Berufspraxis I & Portfolio", 5),
-        new Module("BP5_01.2.HFE", "Berufspraxis II & Portfolio", 5),
-        new Module("BP5_01.3.HFE", "Berufspraxis III & Portfolio", 10),
+        new ModuleModel("BP5_01.1.HFE", "Berufspraxis I & Portfolio", 5),
+        new ModuleModel("BP5_01.2.HFE", "Berufspraxis II & Portfolio", 5),
+        new ModuleModel("BP5_01.3.HFE", "Berufspraxis III & Portfolio", 10),
       ]),
       new Category(
         "Wahlpflichtbereich HFE",
         [
-          new Module("WP2_04.1", "Heilpädagogik im Bereich Hören I", 5),
-          new Module("WP2_04.2", "Heilpädagogik im Bereich Hören II", 5),
-          new Module("WP2_05.1", "Heilpädagogik im Bereich Sehen I", 5),
-          new Module(
+          new ModuleModel("WP2_04.1", "Heilpädagogik im Bereich Hören I", 5),
+          new ModuleModel("WP2_04.2", "Heilpädagogik im Bereich Hören II", 5),
+          new ModuleModel("WP2_05.1", "Heilpädagogik im Bereich Sehen I", 5),
+          new ModuleModel(
             "WP2_06.1",
             "Heilpädagogik im Bereich körperlich-motorische Entwicklung. Motorische Beeinträchtigungen",
             5
           ),
-          new Module(
+          new ModuleModel(
             "WP2_06.2",
             "Heilpädagogik im Bereich körperlich-motorische Entwicklung. Chronische Erkrankungen",
             5
           ),
-          new Module("WP2_07", "Schwere mehrfache Beeinträchtigungen", 5),
-          new Module(
+          new ModuleModel("WP2_07", "Schwere mehrfache Beeinträchtigungen", 5),
+          new ModuleModel(
             "WP2_11",
             "Autismus im Kontext der Heilpädagogischen Früherziehung",
             5
@@ -127,7 +120,7 @@ export default {
         3
       ),
       new Category("Masterarbeit HFE", [
-        new Module("M5_03", "Masterarbeit", 20),
+        new ModuleModel("M5_03", "Masterarbeit", 20),
       ]),
     ];
 
