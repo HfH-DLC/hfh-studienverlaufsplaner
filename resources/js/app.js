ffb4d555b88fe6bbeda05d425e762ff63c9c9433
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2021-11-24 14:41:20 +0100

    Register Components globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,22 @@
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress'
+import Layout from "./Layout.vue"
+
 createInertiaApp({
-    resolve: name => require(`./Pages/${name}`),
+    resolve: async name => {
+        let page = (await
+            import (`./Pages/${name}`)).default;
+        if (!page.layout) {
+            page.layout = Layout;
+        }
+        return page;
+    },
     setup({ el, app, props, plugin }) {
         createApp({ render: () => h(app, props) })
             .use(plugin)
+            .component('Link', Link)
+            .component('Head', Head)
             .mount(el);
     },
 });
