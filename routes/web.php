63a91e40c4145dba05d9e77bd3ceb03a358e1960
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2021-12-07 17:51:20 +0100

    Add endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Http\Resources\ModuleResource;
+use App\Http\Resources\PlanResource;
+use App\Models\Category;
+use App\Models\Module;
+use App\Models\Placement;
+use App\Models\Plan;
+use App\Models\Rule;
+use App\Models\TimeSlot;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 /*
@@ -18,8 +27,97 @@ use Inertia\Inertia;
 */
 Route::name('login')->get('/login', '\\'.Route::getRoutes()->getByName('shibboleth-login')->getActionName());
 
+Route::get('/', function () {
+    return Inertia::render('Home', []);
+})->name('home');
+
+Route::get('/plans/{plan}', function (Plan $plan) {
+    $plan->load('placements');
+    $catogries = Category::all();
+    $timeSlots = TimeSlot::all();
+    $modules = ModuleResource::collection(Module::all());
+    $rules = Rule::all();
+    return Inertia::render('Plan', array('plan' => $plan, 'categories' => $catogries, 'timeSlots' => $timeSlots, 'modules' => $modules, 'rules' => $rules));
+})->name('plan');
+
+Route::post('/plans', function () {
+    $plan = new Plan();
+    $plan->save();
+    return Redirect::route('plan', array('plan' => $plan->id));
+});
+
+Route::put('/plans/{plan}', function (Request $request, Plan $plan) {
+    $attributes = $request->validate([
+        'placements' => ['required'],
+    ]);
+
+    return $plan;
+});
+
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Home', []);
-    })->name('home');
+
+    Route::get('/admin', function () {
+        //todo get values for current planer instead and map to response objects
+        $catogries = Category::all();
+        $timeSlots = TimeSlot::all();
+        $modules = Module::all();
+        return Inertia::render('Admin', ['categories' => $catogries, 'timeSlots' => $timeSlots, 'modules' => $modules]);
+    })->name('admin');
+
+    Route::post('/categories', function (Request $request) {
+        //todo validation
+        $attributes = $request->validate([
+            'name' => ['required'],
+            'requiredNumber' => ['nullable','numeric', 'integer', 'min:0']
+        ]);
+
+        $category = new Category();
+        $category->name = $attributes['name'];
+        $category->required_number = $attributes['requiredNumber'];
+        $category->save();
+        return redirect()->route('admin');
+    });
+
+    Route::post('/timeslots', function (Request $request) {
+        //todo validation
+        $attributes = $request->validate([
+            'year' => ['required'],
+            'semester' => ['required'],
+            'week' => ['required'],
+            'day' => ['required'],
+            'time' => ['required'],
+        ]);
+
+        $timeSlot = new TimeSlot();
+        $timeSlot->year = $attributes['year'];
+        $timeSlot->semester = $attributes['semester'];
+        $timeSlot->week = $attributes['week'];
+        $timeSlot->day = $attributes['day'];
+        $timeSlot->time = $attributes['time'];
+        $timeSlot->save();
+        return redirect()->route('admin');
+    });
+
+    Route::post('/modules', function (Request $request) {
+        //todo validation
+        $attributes = $request->validate([
+            'number' => ['required'],
+            'name' => ['required'],
+            'category' => ['required', 'exists:categories,id'],
+            'credits' => ['required', 'numeric', 'integer', 'min:0'],
+            'timeSlots' => ['required', 'exists:time_slots,id'],
+            'prerequisites' => ['nullable', 'array'],
+        ]);
+
+        $module = new Module();
+        $module->number = $attributes['number'];
+        $module->name = $attributes['name'];
+        $module->category()->associate($attributes['category']);
+        $module->credits = $attributes['credits'];
+        $module->save();
+        $module->timeSlots()->sync($attributes['timeSlots']);
+        //$module->prerequisites()->sync($attributes['prerequisites']);
+        $module->save();
+        return redirect()->route('admin');
+    });
 });
