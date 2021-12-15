d26a0c094f7bab991efb3fb860ab60369f98a8bf
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2021-12-15 15:18:46 +0100

    Remove unused classes

--- resources/js/Models/Category.js
+++
@@ -1,8 +0,0 @@
-export default class Category {
-
-    constructor(id, name, requiredNumber) {
-        this.id = id
-        this.name = name
-        this.requiredNumber = requiredNumber
-    }
-}
--- resources/js/Models/TimeSlot.js
+++
@@ -1,9 +0,0 @@
-export default class TimeSlot {
-    constructor(id, semester, week, day, time) {
-        this.id = id
-        this.semester = semester
-        this.week = week
-        this.day = day
-        this.time = time
-    }
-}
