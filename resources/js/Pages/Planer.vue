f3ea026aaeb66744b171297c55c67518796338a7
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2021-12-20 13:47:14 +0100

    Add form to view existing plan

--- resources/js/Pages/Planer.vue
+++ resources/js/Pages/Planer.vue
@@ -1,22 +1,69 @@
 <template>
-  <div class="w-32 mx-auto">
-    <form @submit.prevent="createPlan">
-      <label for="year" class="text-sm uppercase">Jahr</label>
-      <select
-        v-model="form.startYear"
-        required
-        class="block border border-gray-600 rounded shadow-inner p-1 w-full"
-      >
-        <option value="" disabled>Bitte wählen...</option>
-        <option :value="2021">2021</option>
-        <option :value="2022">2022</option>
-        <option :value="2023">2023</option>
-      </select>
-      <div v-if="form.errors.startYear">{{ form.errors.startYear }}</div>
-      <button class="mt-3 rounded p-2 border-2 border-black font-bold w-full">
-        Plan erstellen
-      </button>
-    </form>
+  <div class="mt-20 w-full p-4 flex justify-around gap-4">
+    <div>
+      <h2 class="text-xl">Ich habe bereits einen Plan.</h2>
+      <div class="w-64">
+        <form @submit.prevent="viewPlan" class="mt-4">
+          <label for="slug" class="text-sm uppercase">Plan-Nummer</label>
+          <input
+            type="text"
+            name="slug"
+            id="slug"
+            class="w-full block border border-gray-600 rounded shadow-inner p-1"
+            placeholder="X63p4Z"
+            v-model="planSlug"
+            required
+          />
+          <button
+            class="
+              w-full
+              mt-3
+              rounded
+              p-2
+              bg-gray-700
+              text-white
+              hover:bg-gray-900
+            "
+          >
+            Anschauen
+          </button>
+        </form>
+      </div>
+    </div>
+    <div>
+      <h2 class="text-xl">Ich möchte einen neuen Plan erstellen.</h2>
+      <div class="mt-4 w-64">
+        <form @submit.prevent="createPlan">
+          <label for="year" class="text-sm uppercase">Jahr</label>
+          <select
+            v-model="createForm.startYear"
+            required
+            class="block border border-gray-600 rounded shadow-inner p-2 w-full"
+          >
+            <option value="" disabled>Bitte wählen...</option>
+            <option :value="2021">2021</option>
+            <option :value="2022">2022</option>
+            <option :value="2023">2023</option>
+          </select>
+          <div v-if="createForm.errors.startYear">
+            {{ createForm.errors.startYear }}
+          </div>
+          <button
+            class="
+              w-full
+              mt-3
+              rounded
+              p-2
+              bg-gray-700
+              text-white
+              hover:bg-gray-900
+            "
+          >
+            Plan erstellen
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,16 +77,20 @@ export default {
   },
   data() {
     return {
-      form: this.$inertia.form({
+      createForm: this.$inertia.form({
         startYear: "",
       }),
+      planSlug: "",
     };
   },
   methods: {
     createPlan() {
-      this.form.post(`/planers/${this.slug}/plans`, {
-        onSuccess: () => this.form.reset(),
+      this.createForm.post(`/planers/${this.slug}/plans`, {
+        onSuccess: () => this.createForm.reset(),
       });
+    },
+    viewPlan() {
+      this.$inertia.get(`/planers/${this.slug}/plans/${this.planSlug}`);
     },
   },
 };
