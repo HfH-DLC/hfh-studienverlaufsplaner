78e49f3425f95485b2d093cfba8bdc2f6c68181d
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2022-01-27 10:48:38 +0100

    Update ModuleInformation.vue

--- resources/js/Components/ModuleInformation.vue
+++ resources/js/Components/ModuleInformation.vue
@@ -1,16 +1,12 @@
 <template>
   <div v-if="selectedModule">
     <h2 class="text-xl">{{ selectedModule.name }}</h2>
-    <ul class="mt-4 space-y-2" v-if="selectedModule.errors.length > 0">
-      <ErrorList :errors="selectedModule.errors" />
+    <ul class="mt-4 space-y-2" v-if="selectedModule.infos.length > 0">
+      <ErrorList :errors="selectedModule.infos" />
     </ul>
     <dl class="mt-4">
       <dt class="text-sm text-gray-500 font-bold uppercase">Modulnummer</dt>
       <dd>{{ selectedModule.number }}</dd>
-      <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">
-        Kreditpunkte
-      </dt>
-      <dd>{{ selectedModule.credits }}</dd>
       <template v-if="selectedModule.prerequisites.length > 0">
         <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">
           Voraussetzungen
@@ -26,20 +22,21 @@
           </ul>
         </dd>
       </template>
-      <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">Termine</dt>
-      <dd>
-        <ul class="space-y-1">
-          <li
-            v-for="(timeSlot, index) in selectedModule.timeSlots"
-            :key="index"
-          >
-            {{ timeSlot.semester }} {{ timeSlot.year }} {{ timeSlot.week }}
-            {{ timeSlot.day }}
-            {{ timeSlot.time }}
-          </li>
-        </ul>
-      </dd>
+      <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">
+        Kreditpunkte
+      </dt>
+      <dd>{{ selectedModule.credits }}</dd>
     </dl>
+    <!-- <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">Termine</dt>
+    <dd>
+      <ul class="space-y-1">
+        <li v-for="(timeSlot, index) in selectedModule.timeSlots" :key="index">
+          {{ timeSlot.semester }} {{ timeSlot.year }}
+          {{ timeSlot.day }}
+          {{ timeSlot.time }}
+        </li>
+      </ul>
+    </dd> -->
   </div>
 </template>
 
