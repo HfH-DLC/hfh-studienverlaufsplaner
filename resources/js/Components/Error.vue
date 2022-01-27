c640ed6378cda51691b2220f85843873b4e568ef
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2022-01-27 10:33:10 +0100

    Change error icon

--- resources/js/Components/Error.vue
+++ resources/js/Components/Error.vue
@@ -2,16 +2,16 @@
   <div
     class="bg-red-50 text-red-600 border border-red-300 rounded-md p-1 w-fit"
   >
-    <ExclamationCircleIcon class="inline-block w-5 h-5 flex-shrink-0 mb-0.5" />
-    <span class="error-message ml-1"><slot></slot></span>
+    <XCircleIcon class="inline-block w-5 h-5 flex-shrink-0 mb-0.5" />
+    <span class="ml-1"><slot></slot></span>
   </div>
 </template>
 
 <script>
-import { ExclamationCircleIcon } from "@heroicons/vue/outline";
+import { XCircleIcon } from "@heroicons/vue/outline";
 export default {
   components: {
-    ExclamationCircleIcon,
+    XCircleIcon,
   },
 };
 </script>
