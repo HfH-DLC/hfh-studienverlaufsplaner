6442de773610b7728e6bc5c72be306f05f8e451e
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2022-01-27 10:39:42 +0100

    Update Navigation.vue

--- resources/js/Components/Navigation.vue
+++ resources/js/Components/Navigation.vue
@@ -1,12 +1,22 @@
 <template>
-  <nav class="flex justify-between items-end p-4 border-b border-gray-300">
+  <nav
+    class="
+      flex
+      justify-between
+      items-end
+      h-full
+      px-4
+      pb-4
+      border-b border-gray-300
+    "
+  >
     <Link :href="`/planers/${$page.props.planerSlug}`"
       ><h1 class="text-3xl">
         Studienverlaufsplaner
         <span v-if="$page.props.planerName">{{ $page.props.planerName }}</span>
       </h1></Link
     >
-    <div v-if="$page.props.plan">
+    <div v-if="$page.props.plan" id="plan-number">
       Plan-Nummer: {{ $page.props.plan.data.slug }}
     </div>
   </nav>
