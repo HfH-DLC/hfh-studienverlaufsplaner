33da9c83e46991f8880bf8639610b1c4518b97e5
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2022-01-27 10:42:32 +0100

    Use Accordion component

--- resources/js/Components/ModuleList.vue
+++ resources/js/Components/ModuleList.vue
@@ -1,68 +1,64 @@
 <template>
-  <Disclosure
+  <Accordion
     v-for="(category, index) in categories"
     :key="category.id"
-    as="div"
-    class="mb-4 border border-gray-300 rounded"
-    v-slot="{ open }"
-    :defaultOpen="index == 0"
+    :open="currentOpen === index"
+    @opened="currentOpen = index"
+    @closed="currentOpen = -1"
   >
-    <DisclosureButton
-      class="
-        flex
-        justify-between
-        items-center
-        w-full
-        px-4
-        py-2
-        text-left
-        bg-gray-50
-        rounded-t
-        focus:outline-none focus:ring-2 focus:ring-indigo-600
-      "
-      :class="{ 'border-b': open, 'rounded-b': !open }"
-    >
-      <h2 class="flex items-center justify-between gap-2">
+    <template v-slot:title>
+      <h2
+        class="flex items-center justify-between gap-2"
+        :class="{
+          'text-green-700': category.placedNumber === category.requiredNumber,
+        }"
+      >
+        <CheckCircleIcon
+          v-if="category.placedNumber === category.requiredNumber"
+          class="text-green-700 w-5 h-5"
+        />
         <div>{{ category.name }}</div>
         <div class="whitespace-nowrap">
           ({{ category.placedNumber }} / {{ category.requiredNumber }})
         </div>
-        <CheckCircleIcon
-          v-if="category.placedNumber === category.requiredNumber"
-          class="text-green-600 w-5 h-5"
-        />
       </h2>
       <ChevronUpIcon
-        :class="open ? 'transform rotate-180' : ''"
+        :class="currentOpen === index ? 'transform rotate-180' : ''"
         class="w-5 h-5 transition"
       />
-    </DisclosureButton>
-    <DisclosurePanel class="p-4">
+    </template>
+    <template v-slot:content>
       <ul>
         <li v-for="module in category.modules" :key="module.id">
           <Module
             :module="module"
-            :disabled="category.placedNumber === category.requiredNumber"
+            :disabled="
+              !module.placed &&
+              category.placedNumber === category.requiredNumber
+            "
           />
         </li>
       </ul>
-    </DisclosurePanel>
-  </Disclosure>
+    </template>
+  </Accordion>
 </template>
 
 <script>
-import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { ChevronUpIcon, CheckCircleIcon } from "@heroicons/vue/outline";
 import Module from "../Components/Module.vue";
+import Accordion from "../Components/Accordion.vue";
 import { mapGetters } from "vuex";
 export default {
   components: {
+    Accordion,
     CheckCircleIcon,
     ChevronUpIcon,
-    Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
     Module,
+  },
+  data() {
+    return {
+      currentOpen: 0,
+    };
   },
   computed: {
     ...mapGetters(["categories"]),
