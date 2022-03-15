b07f83610a70c856ace3a102ecd6bc03c07a1999
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2022-03-15 15:03:15 +0100

    Update CategoryResource.php

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -17,7 +17,7 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'required_number' => $this->whenPivotLoaded('category_planer', function () {
+            'requiredNumber' => $this->whenPivotLoaded('category_planer', function () {
                 return $this->pivot->required_number;
             })
         ];
