a1a36c84adc474a007adb10a9fcacae1770a89bb
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2022-03-28 14:35:01 +0200

    Update Planer.vue

--- resources/js/Pages/Planer.vue
+++ resources/js/Pages/Planer.vue
@@ -1,56 +1,125 @@
 <template>
-  <div class="mt-20 w-full p-4 flex justify-around gap-4">
-    <div>
-      <h2 class="text-xl">Ich habe bereits einen Plan.</h2>
-      <div class="w-64">
-        <form @submit.prevent="viewPlan" class="mt-4">
-          <label for="slug" class="text-sm uppercase">Plan-Nummer</label>
-          <input
-            type="text"
-            name="slug"
-            id="slug"
-            class="w-full block border border-gray-600 rounded shadow-inner p-1"
-            placeholder="X63p4Z"
-            v-model="planSlug"
-            required
-          />
-          <Button class="w-full mt-3">Anschauen</Button>
-        </form>
+  <nav class="flex justify-between items-end p-4 border-b border-gray-300">
+    <Link :href="`/planers/${slug}`"
+      ><h1 class="text-3xl">
+        Studienplanungsinstrument
+        <span v-if="name">{{ name }}</span>
+      </h1></Link
+    >
+  </nav>
+  <div class="p-4 max-w-6xl mx-auto">
+    <h2 class="text-2xl mt-4 mb-4">
+      Willkommen zum Studienplanungsinstrument (Stupli) {{ name }}
+    </h2>
+
+    <p>
+      Hier können Sie Ihren provisorischen Wunsch-Studienverlauf über alle
+      Semester planen. Der zusammengestellte Plan dient als Orientierung! Die
+      Realität sieht künftig allfällig anders aus. Die HfH behält sich vor,
+      Module bei zu geringer Nachfrage abzusagen.
+    </p>
+
+    <h3 class="text-xl mt-3">Wichtig</h3>
+    <p>
+      Dieses Instrument hat nichts mit der offiziellen Modul-Einschreibung für
+      das HfH Studium im daylight-Web zu tun.
+    </p>
+    <div class="my-8 py-8 w-full flex justify-evenly">
+      <div>
+        <h2 class="text-xl">Ich habe bereits einen Plan.</h2>
+        <div>
+          <form @submit.prevent="viewPlan" class="mt-4 w-64">
+            <label for="slug" class="text-sm uppercase">Plan-Nummer</label>
+            <input
+              type="text"
+              name="slug"
+              id="slug"
+              class="
+                leading-4
+                w-full
+                block
+                border border-gray-600
+                rounded
+                shadow-inner
+                p-2
+              "
+              placeholder="X63p4Z"
+              v-model="planSlug"
+              required
+            />
+            <Button class="w-full mt-3">Anschauen</Button>
+          </form>
+        </div>
+      </div>
+      <div>
+        <h2 class="text-xl">Ich möchte einen neuen Plan erstellen.</h2>
+        <div>
+          <form @submit.prevent="createPlan" class="mt-4 w-64">
+            <label for="year" class="text-sm uppercase">Jahr</label>
+            <select
+              v-model="createForm.startYear"
+              required
+              class="
+                block
+                border border-gray-600
+                rounded
+                shadow-inner
+                p-2
+                w-full
+              "
+            >
+              <option value="" disabled>Bitte wählen...</option>
+              <option :value="2022">2022</option>
+            </select>
+            <div v-if="createForm.errors.startYear">
+              {{ createForm.errors.startYear }}
+            </div>
+            <Button class="w-full mt-3">Plan erstellen</Button>
+          </form>
+        </div>
       </div>
     </div>
     <div>
-      <h2 class="text-xl">Ich möchte einen neuen Plan erstellen.</h2>
-      <div class="mt-4 w-64">
-        <form @submit.prevent="createPlan">
-          <label for="year" class="text-sm uppercase">Jahr</label>
-          <select
-            v-model="createForm.startYear"
-            required
-            class="block border border-gray-600 rounded shadow-inner p-2 w-full"
-          >
-            <option value="" disabled>Bitte wählen...</option>
-            <option :value="2022">2022</option>
-          </select>
-          <div v-if="createForm.errors.startYear">
-            {{ createForm.errors.startYear }}
-          </div>
-          <Button class="w-full mt-3">Plan erstellen</Button>
-        </form>
-      </div>
+      <p>
+        <HfHLink :href="degreeLink">Alle Infos zum {{ name }}</HfHLink>
+      </p>
+
+      <p class="mt-2">
+        <HfHLink :href="moduleDirectoryLink">Zum Modulverzeichnis</HfHLink>
+      </p>
+      <p class="mt-4">{{ date }}, Änderungen vorbehalten.</p>
     </div>
   </div>
 </template>
 
 <script>
 import Button from "../Components/Button.vue";
+import HfHLink from "../Components/HfHLink.vue";
 export default {
   components: {
     Button,
+    HfHLink,
   },
   props: {
     slug: {
       type: String,
       required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    degreeLink: {
+      type: String,
+      required: true,
+      default:
+        "https://www.hfh.ch/ausbildung/ma-heilpaedagogische-frueherziehung/auf-einen-blick", //todo get from server
+    },
+    moduleDirectoryLink: {
+      type: String,
+      required: true,
+      default:
+        "https://daylightweb.hfh.ch/modulverzeichnis-1/ma-heilpaedagogische-frueherziehung-hfe", //todo get from server
     },
   },
   data() {
@@ -60,6 +129,11 @@ export default {
       }),
       planSlug: "",
     };
+  },
+  computed: {
+    date() {
+      return new Date().toLocaleDateString();
+    },
   },
   methods: {
     createPlan() {
