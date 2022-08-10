62aac6bd9e2b33925901d70294dc7bd6e7ee7722
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-10 16:15:28 +0200

    Add foci to plan resource

--- app/Http/Resources/PlanResource.php
+++ app/Http/Resources/PlanResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Focus;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PlanResource extends JsonResource
@@ -19,7 +20,9 @@ class PlanResource extends JsonResource
             'placements' => PlacementResource::collection($this->placements),
             'startYear' => $this->start_year,
             'tourCompleted' => $this->tour_completed,
-            'modules' =>  ModuleResource::collection($this->modules)
+            'modules' =>  ModuleResource::collection($this->modules),
+            'firstFocus' => new FocusResource(Focus::find($this->first_focus)),
+            'secondFocus' => new FocusResource(Focus::find($this->second_focus))
         ];
     }
 }
