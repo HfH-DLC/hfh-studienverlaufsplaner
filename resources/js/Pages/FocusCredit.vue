6f45e865a3b03658e6041527f048d864061e24c7
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-17 11:17:23 +0200

    Add focus credit validation

--- resources/js/Pages/FocusCredit.vue
+++ resources/js/Pages/FocusCredit.vue
@@ -2,7 +2,7 @@
   <main class="p-4 max-w-6xl mx-auto">
     <h1 class="text-3xl mt-4 mb-2">Anrechnung an die Studienschwerpunkte</h1>
     <form @submit.prevent="save">
-      <div class="mt-4 grid grid-cols-2 gap-y-4">
+      <div class="mt-4 grid grid-cols-[1fr,auto] gap-y-4">
         <template v-for="module in modules" :key="module.id">
           <div>
             <label :for="`credit-${module.id}`"
@@ -10,26 +10,32 @@
             >
           </div>
           <div v-if="module.fixedCredit">
-            {{ module.creditedAgainst.label }}
+            SSP {{ module.creditedAgainst.label }}
           </div>
           <HfHSelect
             v-else
             :id="`credit-${module.id}`"
             :options="focusOptions"
             v-model="form.focusCredits[module.id]"
+            emptyOptionLabel="Nicht anrechnen"
           />
         </template>
       </div>
+      <ErrorList class="mt-4 space-y-2" :errors="errors" />
       <HfHButton class="mt-4" :disabled="form.processing">Weiter</HfHButton>
     </form>
   </main>
 </template>
 
 <script>
+import ErrorList from "../Components/ErrorList.vue";
 import HfHButton from "../Components/HfHButton.vue";
 import HfHSelect from "../Components/HfHSelect.vue";
+
+import ECTSPerFocusRule from "../Models/Rules/FocusCredit/ECTSPerFocusRule";
+import RequiredModulePerFocusRule from "../Models/Rules/FocusCredit/RequiredModulePerFocusRule";
 export default {
-  components: { HfHButton, HfHSelect },
+  components: { ErrorList, HfHButton, HfHSelect },
   props: {
     creditableModulesResource: {
       type: Object,
@@ -47,44 +53,71 @@ export default {
 
   data() {
     return {
+      errors: [],
       form: this.$inertia.form({
         focusCredits: this.creditableModulesResource.data.reduce((acc, cur) => {
           acc[cur.id] = cur.creditedAgainst ? cur.creditedAgainst.id : "";
           return acc;
         }, {}),
       }),
+      rules: [
+        new ECTSPerFocusRule({ minECTS: 30, maxECTS: 65 }),
+        new RequiredModulePerFocusRule({
+          moduleIds: ["BP5_01.1.SHP", "BP5_01.2.SHP", "BP5_01.3.SHP"],
+        }),
+      ],
     };
   },
 
   computed: {
+    isValid() {
+      return this.errors.length > 0;
+    },
     modules() {
       return this.creditableModulesResource.data;
     },
     focusOptions() {
       return this.planResource.data.focusSelections.map((focusSelection) => ({
-        label: focusSelection.focus.name,
+        label: "SSP " + focusSelection.focus.name,
         value: focusSelection.id,
       }));
     },
   },
 
   methods: {
+    validate() {
+      const focusCredits = this.groupByFocus(this.form.focusCredits);
+      this.errors = [];
+      this.rules.forEach((rule) =>
+        rule.validate(
+          focusCredits,
+          this.planResource.data.focusSelections,
+          this.modules,
+          this.errors
+        )
+      );
+    },
+    groupByFocus(focusCredits) {
+      return Object.entries(focusCredits).reduce((acc, cur) => {
+        const focusId = cur[1];
+        if (!focusId) {
+          return acc;
+        }
+        const moduleId = cur[0];
+        if (!acc[focusId]) {
+          acc[focusId] = [];
+        }
+        acc[focusId].push(moduleId);
+        return acc;
+      }, {});
+    },
     save() {
+      this.validate();
+      if (!this.isValid) {
+        return;
+      }
       this.form
-        .transform((data) => ({
-          focusCredits: Object.entries(data.focusCredits).reduce((acc, cur) => {
-            const focusId = cur[1];
-            if (!focusId) {
-              return acc;
-            }
-            const moduleId = cur[0];
-            if (!acc[focusId]) {
-              acc[focusId] = [];
-            }
-            acc[focusId].push(moduleId);
-            return acc;
-          }, {}),
-        }))
+        .transform((data) => this.groupByFocus(data.focusCredits))
         .put(`/${this.planerSlug}/${this.planResource.data.slug}/anrechnung`, {
           onSuccess: () => this.form.reset(),
         });
