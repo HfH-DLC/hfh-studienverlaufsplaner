4dd0d7ae314b0fff2e2d8b6ef81e74eba26cad37
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-24 17:20:48 +0200

    Update Rules.vue

--- resources/js/Pages/Admin/Rules.vue
+++ resources/js/Pages/Admin/Rules.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="flex-1 flex justify-between">
-        <div class="w-full p-4">
-            <h2 class="text-xl">Regeln</h2>
-            <RulesImport />
-            <EntityList :entities="rules">
-                <template v-slot:label="slotProps">
-                    {{ slotProps.entity.id }} - {{ slotProps.entity.type }} -
-                    {{ slotProps.entity.params }}
-                </template>
-            </EntityList>
-        </div>
+  <div class="flex-1 flex justify-between">
+    <div class="w-full p-4">
+      <h2 class="text-xl">Regeln</h2>
+      <RulesImport />
+      <EntityList :entities="rules">
+        <template v-slot:label="slotProps">
+          {{ slotProps.entity.id }} - {{ slotProps.entity.name }}
+          {{ slotProps.entity.params ? slotProps.entity.params : "" }}
+        </template>
+      </EntityList>
     </div>
+  </div>
 </template>
 
 <script>
@@ -18,26 +18,26 @@ import AdminLayout from "../../Layouts/AdminLayout.vue";
 import EntityList from "../../Components/Admin/EntityList.vue";
 import RulesImport from "../../Components/Admin/RulesImport.vue";
 export default {
-    layout: AdminLayout,
-    components: {
-        EntityList,
-        RulesImport,
+  layout: AdminLayout,
+  components: {
+    EntityList,
+    RulesImport,
+  },
+  props: {
+    rulesResource: {
+      type: Object,
+      required: true,
     },
-    props: {
-        rulesResource: {
-            type: Object,
-            required: true,
-        },
-        types: {
-            type: Array,
-            required: true,
-        },
+    types: {
+      type: Array,
+      required: true,
     },
-    computed: {
-        rules() {
-            return this.rulesResource.data;
-        },
+  },
+  computed: {
+    rules() {
+      return this.rulesResource.data;
     },
+  },
 };
 </script>
 
