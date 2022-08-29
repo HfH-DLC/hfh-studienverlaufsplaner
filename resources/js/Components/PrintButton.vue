c7481224a11cd07b5de8011352ede4b24d73e855
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-29 08:16:44 +0200

    Update PrintButton.vue

--- resources/js/Components/PrintButton.vue
+++ resources/js/Components/PrintButton.vue
@@ -1,25 +1,25 @@
 <template>
-    <div>
-        <button
-            class="p-2 flex items-center gap-1 hover:text-thunderbird-red"
-            @click="print"
-        >
-            <PrinterIcon class="w-7 h-7" aria-hidden="true" /> Drucken
-        </button>
-    </div>
+  <div>
+    <button
+      class="p-2 flex items-center gap-1 hover:text-thunderbird-red"
+      @click="print"
+    >
+      <PrinterIcon class="w-5 h-5" aria-hidden="true" /> Drucken
+    </button>
+  </div>
 </template>
 
 <script>
 import { PrinterIcon } from "@heroicons/vue/outline";
 export default {
-    components: {
-        PrinterIcon,
+  components: {
+    PrinterIcon,
+  },
+  methods: {
+    print() {
+      window.print();
     },
-    methods: {
-        print() {
-            window.print();
-        },
-    },
+  },
 };
 </script>
 
