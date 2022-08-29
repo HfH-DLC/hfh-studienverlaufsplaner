87bfbccc97a50ee59712f6da04ebdbeb93c1f84b
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-29 17:23:12 +0200

    Update RuleFactory.js

--- resources/js/Models/Rules/RuleFactory.js
+++ resources/js/Models/Rules/RuleFactory.js
@@ -1,25 +1,17 @@
 import DateRule from "./Schedule/DateRule";
 import ExcludeSemesterRule from "./Schedule/ExcludeSemesterRule";
 import PrerequisitesRule from "./Schedule/PrerequisitesRule";
-import ECTSPerFocusRule from "./FocusCredit/ECTSPerFocusRule";
-import AtLeastOneOfModulesPerFocus from "./FocusCredit/AtLeastOneOfModulesPerFocusRule";
 
 export const getRule = (state, { name, params }) => {
     switch (name) {
         case "Date": {
             return new DateRule(params);
         }
-        case "ECTSPerFocus": {
-            return new ECTSPerFocusRule(params);
-        }
         case "ExcludeSemester": {
             return new ExcludeSemesterRule(params);
         }
         case "Prerequisites": {
             return new PrerequisitesRule(params);
-        }
-        case "AtLeastOneOfModulesPerFocus": {
-            return new AtLeastOneOfModulesPerFocus(params);
         }
         default:
             throw "Unknown rule: " + name;
