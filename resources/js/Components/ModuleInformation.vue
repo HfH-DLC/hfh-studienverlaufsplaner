5a1226499aa37ea04f474c56705cd6ac7d21922b
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-29 13:11:25 +0200

    Add back button to ModuleInformation

--- resources/js/Components/ModuleInformation.vue
+++ resources/js/Components/ModuleInformation.vue
@@ -1,51 +1,56 @@
 <template>
-    <div v-if="selectedModule">
-        <h2 class="text-xl">{{ selectedModule.name }}</h2>
-        <ErrorList
-            class="mt-4 space-y-2"
-            v-if="selectedModule.infos.length > 0"
-            :errors="selectedModule.infos"
-        />
-        <dl class="mt-4">
-            <dt class="text-sm text-gray-500 font-bold uppercase">
-                Modulnummer
-            </dt>
-            <dd>{{ selectedModule.id }}</dd>
-            <template v-if="selectedModule.prerequisites.length > 0">
-                <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">
-                    Voraussetzungen
-                </dt>
-                <dd>
-                    <ul>
-                        <li
-                            v-for="prerequisite in selectedModule.prerequisites"
-                            :key="prerequisite.id"
-                        >
-                            {{ prerequisite.id }} {{ prerequisite.name }}
-                        </li>
-                    </ul>
-                </dd>
-            </template>
-            <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">
-                Kreditpunkte
-            </dt>
-            <dd>{{ selectedModule.ects }}</dd>
-        </dl>
-    </div>
+  <div v-if="selectedModule">
+    <HfHButton @click="deselectModule" class="mb-4">Zurück</HfHButton>
+    <h2 class="text-xl">{{ selectedModule.name }}</h2>
+    <ErrorList
+      class="mt-4 space-y-2"
+      v-if="selectedModule.infos.length > 0"
+      :errors="selectedModule.infos"
+    />
+    <dl class="mt-4">
+      <dt class="text-sm text-gray-500 font-bold uppercase">Modulnummer</dt>
+      <dd>{{ selectedModule.id }}</dd>
+      <template v-if="selectedModule.prerequisites.length > 0">
+        <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">
+          Voraussetzungen
+        </dt>
+        <dd>
+          <ul>
+            <li
+              v-for="prerequisite in selectedModule.prerequisites"
+              :key="prerequisite.id"
+            >
+              {{ prerequisite.id }} {{ prerequisite.name }}
+            </li>
+          </ul>
+        </dd>
+      </template>
+      <dt class="mt-2 text-sm text-gray-500 font-bold uppercase">
+        Kreditpunkte
+      </dt>
+      <dd>{{ selectedModule.ects }}</dd>
+    </dl>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import ErrorList from "../Components/ErrorList.vue";
+import HfHButton from "../Components/HfHButton";
 export default {
-    components: {
-        ErrorList,
+  components: {
+    ErrorList,
+    HfHButton,
+  },
+  props: {
+    selectedModule: {
+      type: Object,
+      default: null,
     },
-    props: {
-        selectedModule: {
-            type: Object,
-            default: null,
-        },
-    },
+  },
+  methods: {
+    ...mapActions("schedule", ["deselectModule"]),
+  },
 };
 </script>
 
