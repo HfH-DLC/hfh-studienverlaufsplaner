a62c007f7cf84d9094fdfdc109d8bd11e6ec1598
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-29 13:10:27 +0200

    Add header to credit page

--- resources/js/Pages/Credit.vue
+++ resources/js/Pages/Credit.vue
@@ -1,39 +1,45 @@
 <template>
-  <main class="p-4 max-w-6xl mx-auto">
-    <h1 class="text-3xl mt-4 mb-2">Anrechnung an die Studienschwerpunkte</h1>
-    <ErrorList class="mt-4 space-y-2" :errors="errors" />
-    <div class="mt-4 grid grid-cols-[1fr,auto] gap-y-4">
-      <template v-for="module in modules" :key="module.id">
-        <div>
-          <label :for="`credit-${module.id}`"
-            >{{ module.id }} {{ module.name }}</label
-          >
-        </div>
-        <HfHSelect
-          :id="`credit-${module.id}`"
-          :options="focusOptions"
-          :modelValue="module.creditedAgainst ? module.creditedAgainst : ''"
-          @update:modelValue="
-            creditModuleAgainstFocusSelection({
-              moduleId: module.id,
-              focusSelectionId: $event,
-            })
-          "
-          emptyOptionLabel="Nicht anrechnen"
-        />
-      </template>
-    </div>
-  </main>
+  <div class="min-h-screen flex flex-col">
+    <header>
+      <PlanHeader />
+    </header>
+    <main class="flex-1 flex flex-col p-4">
+      <h1 class="text-3xl mt-4 mb-2">Anrechnung an die Studienschwerpunkte</h1>
+      <ErrorList class="mt-4 space-y-2" :errors="errors" />
+      <div class="mt-4 grid grid-cols-[1fr,auto] gap-y-4">
+        <template v-for="module in modules" :key="module.id">
+          <div>
+            <label :for="`credit-${module.id}`"
+              >{{ module.id }} {{ module.name }}</label
+            >
+          </div>
+          <HfHSelect
+            :id="`credit-${module.id}`"
+            :options="focusOptions"
+            :modelValue="module.creditedAgainst ? module.creditedAgainst : ''"
+            @update:modelValue="
+              creditModuleAgainstFocusSelection({
+                moduleId: module.id,
+                focusSelectionId: $event,
+              })
+            "
+            emptyOptionLabel="Nicht anrechnen"
+          />
+        </template>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 import ErrorList from "../Components/ErrorList.vue";
 import HfHButton from "../Components/HfHButton.vue";
 import HfHSelect from "../Components/HfHSelect.vue";
+import PlanHeader from "../Components/PlanHeader.vue";
 
 import { mapActions, mapState } from "vuex";
 export default {
-  components: { ErrorList, HfHButton, HfHSelect },
+  components: { ErrorList, HfHButton, HfHSelect, PlanHeader },
   props: {
     creditableModulesResource: {
       type: Object,
