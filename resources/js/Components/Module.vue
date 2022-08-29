27991010b7357e998e9db2818ac9b87b297090ea
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-08-29 15:40:51 +0200

    Remove ids

--- resources/js/Components/Module.vue
+++ resources/js/Components/Module.vue
@@ -1,76 +1,83 @@
 <template>
-    <button
-        :id="`module-${module.id}`"
-        class="text-left text-sm mb-2 p-1 w-full rounded focus:outline-none focus:ring-2 focus:ring-indigo-600 disabled:cursor-default"
-        :class="{
-            'module--selected': module.selected,
-            'module--placed': module.placement && !module.misplaced,
-            'module--disabled': disabled,
-            'module--error': module.misplaced,
-        }"
-        :disabled="disabled"
-        @click="onClick"
-    >
-        <XCircleIcon
-            v-if="module.misplaced"
-            class="inline-block w-5 h-5 flex-shrink-0 text-red-600"
-        />
-        <CheckCircleIcon
-            v-if="module.placement && !module.misplaced"
-            class="inline-block w-5 h-5 flex-shrink-0 text-green-700"
-        />
-        {{ module.id }} {{ module.name }}
-    </button>
+  <button
+    class="
+      text-left text-sm
+      mb-2
+      p-1
+      w-full
+      rounded
+      focus:outline-none focus:ring-2 focus:ring-indigo-600
+      disabled:cursor-default
+    "
+    :class="{
+      'module--selected': module.selected,
+      'module--placed': module.placement && !module.misplaced,
+      'module--disabled': disabled,
+      'module--error': module.misplaced,
+    }"
+    :disabled="disabled"
+    @click="onClick"
+  >
+    <XCircleIcon
+      v-if="module.misplaced"
+      class="inline-block w-5 h-5 flex-shrink-0 text-red-600"
+    />
+    <CheckCircleIcon
+      v-if="module.placement && !module.misplaced"
+      class="inline-block w-5 h-5 flex-shrink-0 text-green-700"
+    />
+    {{ module.id }} {{ module.name }}
+  </button>
 </template>
 
 <script>
 import { XCircleIcon, CheckCircleIcon } from "@heroicons/vue/outline";
 import { mapActions } from "vuex";
 export default {
-    components: {
-        CheckCircleIcon,
-        XCircleIcon,
+  components: {
+    CheckCircleIcon,
+    XCircleIcon,
+  },
+  props: {
+    module: {
+      type: Object,
+      required: true,
     },
-    props: {
-        module: {
-            type: Object,
-            required: true,
-        },
-        disabled: {
-            type: Boolean,
-            required: true,
-        },
+    disabled: {
+      type: Boolean,
+      required: true,
     },
-    computed: {
-        hasInfos() {
-            return this.module.infos.length > 0;
-        },
+  },
+  computed: {
+    hasInfos() {
+      return this.module.infos.length > 0;
     },
-    methods: {
-        ...mapActions("schedule", ["deselectModule", "selectModule"]),
-        onClick() {
-            if (this.module.selected) {
-                this.deselectModule();
-            } else {
-                this.selectModule(this.module.id);
-            }
-        },
+  },
+  methods: {
+    ...mapActions("schedule", ["deselectModule", "selectModule"]),
+    onClick() {
+      if (this.module.selected) {
+        this.deselectModule();
+      } else {
+        this.selectModule(this.module.id);
+      }
     },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .module--selected {
-    @apply font-bold;
-    @apply tracking-tighter;
+  @apply font-bold;
+  @apply tracking-tighter;
 }
 .module--error {
-    @apply text-red-600;
+  @apply text-red-600;
 }
 .module--placed {
-    @apply text-green-700;
+  @apply text-green-700;
 }
 .module--disabled {
-    @apply text-gray-400;
+  @apply text-gray-400;
 }
 </style>
