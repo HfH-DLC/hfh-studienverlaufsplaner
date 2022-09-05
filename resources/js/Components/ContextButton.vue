60d9741ac02da403aa6754cc9050de63ba25cf77
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-09-05 08:30:44 +0200

    Create ContextButton.vue

---
+++ resources/js/Components/ContextButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="
+      flex
+      justify-center
+      items-center
+      p-2
+      rounded-full
+      z-10
+      bg-gray-700
+      text-white
+      hover:bg-gray-900
+      focus:bg-gray-900
+      shadow
+    "
+  >
+    <div class="sr-only">{{ label }}</div>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
