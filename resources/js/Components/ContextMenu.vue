f1f3c4ea94dcf8d5431ee2eb9e890456d522dcc7
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-09-05 08:30:50 +0200

    Create ContextMenu.vue

---
+++ resources/js/Components/ContextMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex gap-x-1">
+    <ContextButton @click="removeModule(placement)" label="Modul entfernen">
+      <TrashIcon class="w-4 h-4" />
+    </ContextButton>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import ContextButton from "./ContextButton.vue";
+import { TrashIcon } from "@heroicons/vue/outline";
+export default {
+  components: {
+    ContextButton,
+    TrashIcon,
+  },
+  props: {
+    placement: {
+      type: Object,
+      requried: true,
+    },
+  },
+  methods: {
+    ...mapActions("schedule", ["removeModule"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
