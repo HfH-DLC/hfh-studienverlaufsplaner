80203bd38fa9633229baeb89178f7038bbbf60e7
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-09-12 12:24:52 +0200

    Update Credit.vue

--- resources/js/Pages/Credit.vue
+++ resources/js/Pages/Credit.vue
@@ -9,47 +9,54 @@
         :showTour="!!tour"
       />
     </header>
-    <main class="flex-1 flex flex-col p-4">
-      <h1 class="text-3xl mt-4 mb-2">Anrechnung an die Studienschwerpunkte</h1>
+    <main class="flex-1 flex flex-col px-4 pb-4">
       <ErrorList class="mt-4 space-y-2" :errors="errors" />
       <div class="flex mt-4 gap-x-8">
         <template v-if="planResource.data.scheduleValid">
-          <table class="w-9/12" id="modules">
-            <template v-for="(module, index) in modules" :key="module.id">
-              <tr
-                :class="{
-                  'bg-gray-300': index % 2 == 0,
-                  'bg-gray-100': index % 2 != 0,
-                }"
-              >
-                <td class="p-4 whitespace-nowrap w-full">
-                  <label :for="`credit-${module.id}`"
-                    >{{ module.id }} {{ module.name }}</label
-                  >
-                </td>
-                <td class="p-4">
-                  <div v-if="module.requiredCredit">
-                    {{ getFocusName(module.creditedAgainst) }}
-                  </div>
-                  <HfhSelect
-                    v-else
-                    class="w-fit"
-                    :id="`credit-${module.id}`"
-                    :options="focusOptions"
-                    :modelValue="
-                      module.creditedAgainst ? module.creditedAgainst : ''
-                    "
-                    @update:modelValue="
-                      creditModuleAgainstFocusSelection({
-                        moduleId: module.id,
-                        focusSelectionId: $event,
-                      })
-                    "
-                    defaultOption="Nicht anrechnen"
-                  />
-                </td>
+          <table
+            class="w-full divide-y divide-gray-300 border border-gray-300"
+            id="modules"
+          >
+            <caption class="text-white py-2 bg-gray-900">
+              Anrechnung an die Studienschwerpunkte
+            </caption>
+            <thead class="bg-gray-50">
+              <tr class="divide-x divide-gray-300">
+                <th class="px-6 py-2 text-xs text-gray-500">Modul</th>
+                <th class="px-6 py-2 text-xs text-gray-500">Anrechnung</th>
               </tr>
-            </template>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-300">
+              <template v-for="module in modules" :key="module.id">
+                <tr class="divide-x divide-gray-300">
+                  <td class="p-4 w-full">
+                    <label :for="`credit-${module.id}`"
+                      >{{ module.id }} {{ module.name }}</label
+                    >
+                  </td>
+                  <td class="p-4">
+                    <div v-if="module.requiredCredit">
+                      SSP {{ getFocusName(module.creditedAgainst) }}
+                    </div>
+                    <HfhSelect
+                      v-else
+                      :id="`credit-${module.id}`"
+                      :options="focusOptions"
+                      :modelValue="
+                        module.creditedAgainst ? module.creditedAgainst : ''
+                      "
+                      @update:modelValue="
+                        creditModuleAgainstFocusSelection({
+                          moduleId: module.id,
+                          focusSelectionId: $event,
+                        })
+                      "
+                      defaultOption="Nicht anrechnen"
+                    />
+                  </td>
+                </tr>
+              </template>
+            </tbody>
           </table>
           <Checklist :entries="todoEntries" id="todos" class="w-3/12" />
         </template>
@@ -163,4 +170,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+/deep/ .hfh-select {
+  width: fit-content;
+}
+</style>
