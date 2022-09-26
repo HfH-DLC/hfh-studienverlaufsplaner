eb57836a17044b7288c1d762652212113b3a99ea
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-09-26 12:50:52 +0200

    Update SaveStatus.vue

--- resources/js/Components/SaveStatus.vue
+++ resources/js/Components/SaveStatus.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="flex gap-1 items-center" :class="messageClass">
-        <CheckCircleIcon
-            class="inline-block w-5 h-5 flex-shrink-0 text-green-700"
-            v-if="saved"
-        />
-        <div
-            v-if="saving"
-            class="w-5 h-5 border-2 border-transparent border-t-thunderbird-red rounded-full animate-spin"
-        ></div>
-        {{ message }}
-    </div>
+  <div class="flex gap-1 items-center" :class="messageClass">
+    <CheckCircleIcon
+      class="inline-block w-5 h-5 flex-shrink-0 text-green-700"
+      v-if="saved"
+    />
+    <div
+      v-if="saving"
+      class="
+        w-5
+        h-5
+        border-2 border-transparent border-t-thunderbird-red
+        rounded-full
+        animate-spin
+      "
+    ></div>
+    {{ message }}
+  </div>
 </template>
 
 <script>
@@ -18,36 +24,36 @@ import { CheckCircleIcon } from "@heroicons/vue/outline";
 import { SAVE_STATUS_SAVED, SAVE_STATUS_SAVING } from "../constants";
 
 export default {
-    components: {
-        CheckCircleIcon,
+  components: {
+    CheckCircleIcon,
+  },
+  computed: {
+    ...mapState("schedule", ["saveStatus"]),
+    saved() {
+      return this.saveStatus == SAVE_STATUS_SAVED;
     },
-    computed: {
-        ...mapState("schedule", ["saveStatus"]),
-        saved() {
-            return this.saveStatus == SAVE_STATUS_SAVED;
-        },
-        saving() {
-            return this.saveStatus == SAVE_STATUS_SAVING;
-        },
-        message() {
-            if (this.saving) {
-                return "Wird gespeichert...";
-            }
-            if (this.saved) {
-                return "Gespeichert";
-            }
-            return "";
-        },
-        messageClass() {
-            if (this.saving) {
-                return "";
-            }
-            if (this.saved) {
-                return "text-green-700";
-            }
-            return "";
-        },
+    saving() {
+      return this.saveStatus == SAVE_STATUS_SAVING;
     },
+    message() {
+      if (this.saving) {
+        return "Wird gespeichert...";
+      }
+      if (this.saved) {
+        return "Gespeichert";
+      }
+      return "";
+    },
+    messageClass() {
+      if (this.saving) {
+        return "";
+      }
+      if (this.saved) {
+        return "text-green-700";
+      }
+      return "";
+    },
+  },
 };
 </script>
 
