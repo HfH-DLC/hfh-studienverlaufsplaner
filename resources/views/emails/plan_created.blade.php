a6b894c346db4f89125ec3941b79b2071837674f
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-10-17 17:02:51 +0200

    Update plan_created.blade.php

--- resources/views/emails/plan_created.blade.php
+++ resources/views/emails/plan_created.blade.php
@@ -1,4 +1,4 @@
 <body>
     <p>Guten Tag</p>
-<p>Sie haben den HfH-Studienverlaufsplaner genutzt, um Ihr Studium zu planen. Sie können  <a href="{{$url}}">Ihre Planung</a> jederzeit online abrufen und ändern. Bei Fragen technischer Art, wenden Sie sich bitte an <a href="mailto:dlc@hfh.ch">dlc@hfh.ch</a>.  Nehmen Sie beim Planen unbedingt <a href="{{$brochure_url}}">Studienbroschüre</a> und <a href="{{$module_directory_url}}">Modulverzeichnis zur Hand</a></p>
+<p>Sie haben den HfH-Studienverlaufsplaner genutzt, um Ihr Studium zu planen. Sie können  <a href="{{$url}}">Ihre Planung</a> jederzeit online abrufen und ändern. Bei Fragen technischer Art, wenden Sie sich bitte an <a href="mailto:dlc@hfh.ch">dlc@hfh.ch</a>.  Nehmen Sie beim Planen unbedingt <a href="{{$brochure_url}}">Studienbroschüre</a> und <a href="{{$module_directory_url}}">Modulverzeichnis</a> zur Hand.</p>
 </body>
