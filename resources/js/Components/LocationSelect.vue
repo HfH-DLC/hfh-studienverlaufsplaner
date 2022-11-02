f79c885bde51422d38cfc25ccfda2f4faba13901
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-11-02 16:52:02 +0100

    Update LocationSelect.vue

--- resources/js/Components/LocationSelect.vue
+++ resources/js/Components/LocationSelect.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <fieldset class="flex items-center gap-x-4">
-      <legend>Standorte</legend>
+    <fieldset class="flex items-center gap-x-8">
+      <legend class="hfh-label">Standorte</legend>
       <div v-for="(option, index) in selectableLocations" :key="index">
-        <label
+        <label class="flex items-center gap-x-4"
           ><input
             type="checkbox"
             :value="option.id"
             :checked="option.checked"
             @change="onChange($event, index)"
             :disabled="option.checked && checkedLocations.length == 1"
+            class="accent-thunderbird-red w-5 h-5"
           />
           {{ option.name }}</label
         >
