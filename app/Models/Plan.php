95cd79920f540d11f9ffc3724cfe7272e6007eb2
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-02-08 16:14:38 +0100

    Correctly cast credit_valid field to boolean

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -11,7 +11,7 @@ class Plan extends Model
 {
     use HasFactory;
 
-    protected $casts = ['start_year' => 'integer', 'schedule_tour_completed' => 'boolean', 'credit_tour_completed' => 'boolean',  'read_only' => 'boolean', 'schedule_valid' => 'boolean'];
+    protected $casts = ['start_year' => 'integer', 'schedule_tour_completed' => 'boolean', 'credit_tour_completed' => 'boolean',  'read_only' => 'boolean', 'schedule_valid' => 'boolean',  'credit_valid' => 'boolean'];
 
     public function planer()
     {
