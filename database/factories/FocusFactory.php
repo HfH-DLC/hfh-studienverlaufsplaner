31b7fe32014acb96d57ba2b6c00c12d7efaba0ed
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-02-08 15:36:58 +0100

    Create FocusFactory.php

---
+++ database/factories/FocusFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Focus>
+ */
+class FocusFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'id' => 'SSP_XY',
+            'name' => $this->faker->unique()->name(),
+        ];
+    }
+}
