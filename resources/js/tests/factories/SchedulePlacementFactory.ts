c1eac4bc0cc9e54466937c0675af3e84f8934eda
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-06-06 14:44:20 +0200

    Create SchedulePlacementFactory.ts

---
+++ resources/js/tests/factories/SchedulePlacementFactory.ts
@@ -0,0 +1,33 @@
+import { ErrorMessage, SchedulePlacement } from "@/types";
+import * as Factory from "factory.ts";
+import { scheduleModuleFactory } from "./ScheduleModuleFactory";
+import { locationFactory } from "./LocationFactory";
+import { dayTimeFactory } from "./DayTimeFactory";
+
+const schedulePlacementPartialFactory =
+    Factory.Sync.makeFactory<SchedulePlacement>({
+        id: Factory.each((i) => i),
+        location: locationFactory.build(),
+        year: 1990,
+        semester: "HS",
+        dayTime: dayTimeFactory.build(),
+        timeWindow: "TestTimeWindow",
+        moduleId: Factory.each((i) => `test_module_${i}`),
+        module: Factory.each((i) =>
+            scheduleModuleFactory.build({
+                id: `test_module_${i}`,
+            })
+        ),
+        errors: [] as Array<ErrorMessage>,
+    });
+
+export const schedulePlacementFactory =
+    schedulePlacementPartialFactory.withSelfDerivation(
+        "module",
+        (schedulePlacement) => {
+            return {
+                ...schedulePlacement.module,
+                placement: schedulePlacement,
+            };
+        }
+    );
