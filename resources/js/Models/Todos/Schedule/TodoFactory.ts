f3bb5a18f675a21082c8562d7ed03496a2200cf6
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-06-06 10:14:45 +0200

    Update TodoFactory.ts

--- resources/js/Models/Todos/Schedule/TodoFactory.ts
+++ resources/js/Models/Todos/Schedule/TodoFactory.ts
@@ -5,6 +5,17 @@ import FocusModulesTodo from "./FocusModulesTodo";
 import RequiredModulesTodo from "./RequiredModulesTodo";
 import TotalECTSTodo from "./TotalECTSTodo";
 
+export const getTodos = (todosData: Array<TodoData>) => {
+    return todosData.reduce((array: Array<Todo>, todoData) => {
+        try {
+            array.push(getTodo(todoData));
+        } catch (error) {
+            console.error(error);
+        }
+        return array;
+    }, []);
+};
+
 export const getTodo = ({ name }: TodoData): Todo => {
     switch (name) {
         case "AtLeastOneFocusTodo": {
