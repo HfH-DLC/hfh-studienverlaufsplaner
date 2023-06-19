bfca089f118856c185f7718bf8c4820089210f57
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-06-19 09:21:38 +0200

    Update PrerequisitesRule.test.ts

--- resources/js/tests/rules/PrerequisitesRule.test.ts
+++ resources/js/tests/rules/PrerequisitesRule.test.ts
@@ -1,9 +1,10 @@
 import PrerequisitesRule from "@/Models/Rules/Schedule/PrerequisitesRule";
 import { describe, it, expect } from "vitest";
 import { ref } from "vue";
-import { SchedulePlacement } from "@/types";
+import { ErrorMessage, SchedulePlacement } from "@/types";
 import { schedulePlacementFactory } from "@/tests/factories/SchedulePlacementFactory";
 import { scheduleModuleFactory } from "@/tests/factories/ScheduleModuleFactory";
+import { eventFactory } from "../factories/EventFactory";
 
 describe("PrerequisitesRule", () => {
     describe("validatePlacements", () => {
@@ -236,6 +237,184 @@ describe("PrerequisitesRule", () => {
             rule.validatePlacements(data, errorMessages);
 
             expect(errorMessages.size).toBe(0);
+        });
+    });
+
+    describe("validateModule", () => {
+        it("should return no errors if the module has no prerequisites", () => {
+            const rule = new PrerequisitesRule();
+            const errorMessages = new Array<ErrorMessage>();
+            const data = { placements: ref([]) };
+            const module = scheduleModuleFactory.build({ prerequisites: [] });
+
+            rule.validateModule(module, data, errorMessages);
+
+            expect(errorMessages.length).toBe(0);
+        });
+
+        it("should return an error if a module with prerequisites has no events", () => {
+            const rule = new PrerequisitesRule();
+            const errorMessages = new Array<ErrorMessage>();
+            const data = { placements: ref([]) };
+            const prerequisite = scheduleModuleFactory.build();
+            const moduleWithPrerequisites = scheduleModuleFactory.build({
+                prerequisites: [prerequisite],
+            });
+
+            rule.validateModule(moduleWithPrerequisites, data, errorMessages);
+
+            expect(errorMessages.length).toBe(1);
+        });
+
+        it("should return no error if some of a module's events meets the module's prerequisites", () => {
+            const rule = new PrerequisitesRule();
+            const errorMessages = new Array<ErrorMessage>();
+
+            const prerequisite = scheduleModuleFactory.build();
+            const prerequisitePlacement: SchedulePlacement =
+                schedulePlacementFactory.build({
+                    moduleId: prerequisite.id,
+                    module: prerequisite,
+                    year: 1992,
+                    semester: "HS",
+                });
+            prerequisite.placement = prerequisitePlacement;
+            const data = { placements: ref([prerequisitePlacement]) };
+            const moduleWithPrerequisites = scheduleModuleFactory.build({
+                prerequisites: [prerequisite],
+                events: [
+                    eventFactory.build({
+                        year: 1991,
+                        semester: "HS",
+                    }),
+                    eventFactory.build({
+                        year: 1993,
+                        semester: "FS",
+                    }),
+                ],
+            });
+
+            rule.validateModule(moduleWithPrerequisites, data, errorMessages);
+
+            expect(errorMessages.length).toBe(0);
+        });
+
+        it("should return an error if none of a module's events meets the module's prerequisites", () => {
+            const rule = new PrerequisitesRule();
+            const errorMessages = new Array<ErrorMessage>();
+
+            const prerequisite = scheduleModuleFactory.build();
+            const prerequisitePlacement: SchedulePlacement =
+                schedulePlacementFactory.build({
+                    moduleId: prerequisite.id,
+                    module: prerequisite,
+                    year: 1992,
+                    semester: "HS",
+                });
+            prerequisite.placement = prerequisitePlacement;
+            const data = { placements: ref([prerequisitePlacement]) };
+            const moduleWithPrerequisites = scheduleModuleFactory.build({
+                prerequisites: [prerequisite],
+                events: [
+                    eventFactory.build({
+                        year: 1992,
+                        semester: "HS",
+                    }),
+                    eventFactory.build({
+                        year: 1991,
+                        semester: "FS",
+                    }),
+                ],
+            });
+
+            rule.validateModule(moduleWithPrerequisites, data, errorMessages);
+
+            expect(errorMessages.length).toBe(1);
+        });
+    });
+
+    describe("validateSelection", () => {
+        it("should return nothing if the module has no prerequisites", () => {
+            const rule = new PrerequisitesRule();
+            const infos = new Map();
+            const data = { placements: ref([]) };
+            const module = scheduleModuleFactory.build({ prerequisites: [] });
+
+            rule.validateSelection(module, data, infos);
+
+            expect(infos.size).toBe(0);
+        });
+        it("should return nothing for events that meet the prerequisites", () => {
+            const rule = new PrerequisitesRule();
+            const infos = new Map();
+            const prerequisite = scheduleModuleFactory.build();
+            const prerequisitePlacement: SchedulePlacement =
+                schedulePlacementFactory.build({
+                    moduleId: prerequisite.id,
+                    module: prerequisite,
+                    year: 1992,
+                    semester: "HS",
+                });
+            prerequisite.placement = prerequisitePlacement;
+            const data = { placements: ref([prerequisitePlacement]) };
+            const moduleWithPrerequisites = scheduleModuleFactory.build({
+                prerequisites: [prerequisite],
+                events: [
+                    eventFactory.build({
+                        year: 1992,
+                        semester: "FS",
+                    }),
+                    eventFactory.build({
+                        year: 1993,
+                        semester: "FS",
+                    }),
+                ],
+            });
+
+            rule.validateSelection(moduleWithPrerequisites, data, infos);
+
+            expect(infos.size).toBe(0);
+        });
+        it("should return dateAllowed=false for each event that does not meet the prerequisites", () => {
+            const rule = new PrerequisitesRule();
+            const errorMessages = new Map();
+            const prerequisite = scheduleModuleFactory.build();
+            const prerequisitePlacement: SchedulePlacement =
+                schedulePlacementFactory.build({
+                    moduleId: prerequisite.id,
+                    module: prerequisite,
+                    year: 1992,
+                    semester: "HS",
+                });
+            prerequisite.placement = prerequisitePlacement;
+            const data = { placements: ref([prerequisitePlacement]) };
+            const moduleWithPrerequisites = scheduleModuleFactory.build({
+                prerequisites: [prerequisite],
+                events: [
+                    eventFactory.build({
+                        id: 1,
+                        year: 1992,
+                        semester: "HS",
+                    }),
+                    eventFactory.build({
+                        id: 2,
+                        year: 1991,
+                        semester: "FS",
+                    }),
+                ],
+            });
+
+            rule.validateSelection(
+                moduleWithPrerequisites,
+                data,
+                errorMessages
+            );
+
+            expect(errorMessages.size).toBe(2);
+            expect(errorMessages.get(1).valid).toBe(true);
+            expect(errorMessages.get(1).dateAllowed).toBe(false);
+            expect(errorMessages.get(2).valid).toBe(true);
+            expect(errorMessages.get(2).dateAllowed).toBe(false);
         });
     });
 });
