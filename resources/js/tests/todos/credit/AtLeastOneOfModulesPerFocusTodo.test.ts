3a4a8a2e48462c327086f673fadd04a9b2d37d56
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-06-20 09:06:53 +0200

    Update AtLeastOneOfModulesPerFocusTodo

--- resources/js/tests/todos/credit/AtLeastOneOfModulesPerFocusTodo.test.ts
+++ resources/js/tests/todos/credit/AtLeastOneOfModulesPerFocusTodo.test.ts
@@ -46,53 +46,53 @@ describe("AtLeastOneOfModulesPerFocusTodo", () => {
             expect(entries[0].checked).toBe(false);
             expect(entries[0].progressLabel).toBe("0");
         });
-    });
 
-    it("should return an entry with checked = true for a focusSelection which has one of the required modules credited against it", () => {
-        const requiredModuleIds: Array<String> = ["req_mod_1", "req_mod_2"];
-        const todo = new AtLeastOneOfModulesPerFocusTodo({
-            moduleIds: requiredModuleIds,
+        it("should return an entry with checked = true for a focusSelection which has one of the required modules credited against it", () => {
+            const requiredModuleIds: Array<String> = ["req_mod_1", "req_mod_2"];
+            const todo = new AtLeastOneOfModulesPerFocusTodo({
+                moduleIds: requiredModuleIds,
+            });
+            const data = {
+                focusSelections: ref([focusSelectionFactory.build({ id: 1 })]),
+                modules: ref([
+                    creditModuleFactory.build({
+                        id: "req_mod_1",
+                        creditedAgainst: 1,
+                    }),
+                ]),
+            };
+
+            const entries = todo.getEntries(data);
+
+            expect(entries.length).toBe(1);
+            expect(entries[0].checked).toBe(true);
+            expect(entries[0].progressLabel).toBe("1");
         });
-        const data = {
-            focusSelections: ref([focusSelectionFactory.build({ id: 1 })]),
-            modules: ref([
-                creditModuleFactory.build({
-                    id: "req_mod_1",
-                    creditedAgainst: 1,
-                }),
-            ]),
-        };
 
-        const entries = todo.getEntries(data);
+        it("should return an entry with checked = true for a focusSelection which has more than one of the required modules credited against it", () => {
+            const requiredModuleIds: Array<String> = ["req_mod_1", "req_mod_2"];
+            const todo = new AtLeastOneOfModulesPerFocusTodo({
+                moduleIds: requiredModuleIds,
+            });
+            const data = {
+                focusSelections: ref([focusSelectionFactory.build({ id: 1 })]),
+                modules: ref([
+                    creditModuleFactory.build({
+                        id: "req_mod_1",
+                        creditedAgainst: 1,
+                    }),
+                    creditModuleFactory.build({
+                        id: "req_mod_2",
+                        creditedAgainst: 1,
+                    }),
+                ]),
+            };
 
-        expect(entries.length).toBe(1);
-        expect(entries[0].checked).toBe(true);
-        expect(entries[0].progressLabel).toBe("1");
-    });
+            const entries = todo.getEntries(data);
 
-    it("should return an entry with checked = true for a focusSelection which has more than one of the required modules credited against it", () => {
-        const requiredModuleIds: Array<String> = ["req_mod_1", "req_mod_2"];
-        const todo = new AtLeastOneOfModulesPerFocusTodo({
-            moduleIds: requiredModuleIds,
+            expect(entries.length).toBe(1);
+            expect(entries[0].checked).toBe(true);
+            expect(entries[0].progressLabel).toBe("2");
         });
-        const data = {
-            focusSelections: ref([focusSelectionFactory.build({ id: 1 })]),
-            modules: ref([
-                creditModuleFactory.build({
-                    id: "req_mod_1",
-                    creditedAgainst: 1,
-                }),
-                creditModuleFactory.build({
-                    id: "req_mod_2",
-                    creditedAgainst: 1,
-                }),
-            ]),
-        };
-
-        const entries = todo.getEntries(data);
-
-        expect(entries.length).toBe(1);
-        expect(entries[0].checked).toBe(true);
-        expect(entries[0].progressLabel).toBe("2");
     });
 });
