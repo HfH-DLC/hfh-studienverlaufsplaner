a49c78c0ccff9e2dcc91b90aac6f3891c82ff261
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-06-26 16:06:54 +0200

    Update PriorLearningFactory.ts

--- resources/js/tests/factories/PriorLearningFactory.ts
+++ resources/js/tests/factories/PriorLearningFactory.ts
@@ -4,6 +4,5 @@ import * as Factory from "factory.ts";
 export const priorLearnignFactory = Factory.Sync.makeFactory<PriorLearning>({
     id: Factory.each((i) => i),
     name: Factory.each((i) => `Test Prior Learning ${i}`),
-    ects: 0,
-    countsAsModuleId: "",
+    ects: 5,
 });
