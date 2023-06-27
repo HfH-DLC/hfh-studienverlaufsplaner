ea5bb1418ed370b0a31b6b86fbef133305c34e7a
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-06-27 10:02:08 +0200

    Create ModuleFactory.ts

---
+++ resources/js/tests/factories/ModuleFactory.ts
@@ -0,0 +1,10 @@
+import { Module } from "@/types";
+import * as Factory from "factory.ts";
+
+export const moduleFactory = Factory.Sync.makeFactory<Module>({
+    id: Factory.each((i) => `TEST_${i}`),
+    name: Factory.each((i) => `Testmodule_${i}`),
+    events: [],
+    ects: 5,
+    prerequisites: [],
+});
